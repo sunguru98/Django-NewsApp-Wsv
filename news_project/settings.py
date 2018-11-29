614fdedd9805cbd667775da71036931c1362c0b5
sundeep <sunguru98@yahoo.co.in>
2018-11-29 13:55:56 +0530

    Added Custom User Model and added that model to USERADMIN and used CUSTOMUSERCHANGE and CREATE forms

--- news_project/settings.py
+++ news_project/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+TEMPLATE_DIR = os.path.join(DBASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'news_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+AUTH_USER_MODEL = 'users.CustomUser'
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+from .forms import CustomUserChangeForm, CutomUserCreationForm
 # Register your models here.
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CutomUserCreationForm
+    form = CustomUserChangeForm
+    list_display = ["username", "email", "user_age"]
+    model = CustomUser
+
+admin.site.register(CustomUser, CustomUserAdmin)
